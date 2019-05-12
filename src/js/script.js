e9440745e42b8862c28465760c2ac7041a5caad6
Tomek <esemku@icloud.com>
2019-05-12 13:49:01 +0200

    Finishing project

--- src/js/script.js
+++ src/js/script.js
@@ -218,6 +218,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
+      thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
 
@@ -238,11 +239,12 @@
       const thisWidget = this;
 
       const newValue = parseInt(value);
-
       /* TODO: Add validation */
+      if( newValue !== thisWidget.value && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax ) {
+        thisWidget.value = newValue;
+        thisWidget.announce();
+      }
 
-      thisWidget.value = newValue;
-      thisWidget.announce();
       thisWidget.input.value = thisWidget.value;
     }
 
